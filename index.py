520628f7d84af9eb094003a4d6400afb412ddc0b
luk <lalaluk5@ntub.edu.tw>
2022-12-19 22:09:21 +0800

    change folder

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
+from chatgpt import ChatGPT
 
 import os
 
